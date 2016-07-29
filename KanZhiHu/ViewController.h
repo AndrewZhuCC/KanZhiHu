891dd99cfb2255b58e3a1d037da3c604e8f868ed
Andrew <zaz92537@126.com>
2016-07-29 17:24:39 +0800

    Initial Commit

---
+++ KanZhiHu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KanZhiHu
+//
+//  Created by 朱安智 on 16/7/29.
+//  Copyright © 2016年 朱安智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KanZhiHu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KanZhiHu
+//
+//  Created by 朱安智 on 16/7/29.
+//  Copyright © 2016年 朱安智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
